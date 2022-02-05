7c169819a0cf2112385c2a2b80780ac5768aa92c
nguyen chien <quangminhb53@gmail.com>
2022-02-05 19:53:22 +0700

    Project 2

---
+++ vue/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+import store from './store'
+
+
+import router from './router'
+
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app')
---
+++ vue/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {},
+    actions: {},
+    getters: {},
+    mutations: {},
+    modules: {},
+
+})
+
+export default store;
